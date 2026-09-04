6ff380d20ac5e13dda212813d0e1b126f2460d45
Ida-Lotte Peltoniemi <ida.peltoniemi@gmail.com>
2026-09-04 08:35:10 +0200

    create script

---
+++ open_dcm.py
@@ -0,0 +1,20 @@
+import matplotlib
+matplotlib.use('TkAgg')  # fixes some freezes?
+import matplotlib.pyplot as plt
+import pydicom 
+
+file_path = "/Users/ida-lotte/git-exercise/Dataset_Skills_01/CT/1.3.12.2.1107.5.1.4.105055.30000018070216565044500079806/1.3.12.2.1107.5.1.4.105055.30000018070216565044500079807.dcm"
+
+def load_dicom_image(path):
+    # Load the DICOM file
+    dicom_data = pydicom.dcmread(path)
+    
+    # Extract the pixel array
+    image_array = dicom_data.pixel_array
+
+    #show the image
+    plt.imshow(image_array, cmap='gray')
+    plt.title('DICOM Image')
+    plt.show()
+
+load_dicom_image(file_path)
